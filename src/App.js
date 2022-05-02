316e4891e9a8d64ecad15d1a0e7bdcee50d26735
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-05-02 22:19:21 +0200

    Loading everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import './App.css';
+import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
+import WalletAddress from './components/WalletAddress';
+import Menu from './components/Menu';
 
 function App() {
+  const { status, connect, disconnect, availableConnectTypes } = useWallet();
+
+  console.log("Wallet status is ", status);
+  console.log("Available connection types:", availableConnectTypes);
+
+  const renderConnectButton = () => {
+    if (status === WalletStatus.WALLET_NOT_CONNECTED) {
+      return (
+        <div className="connect-wallet-div">
+          <button
+            type="button"
+	    key={`connect-EXTENSION`}
+	    onClick={() => connect("EXTENSION")}
+	    className="cta-button connect-wallet-button"
+	>
+	    Connect wallet
+	</button>
+      </div>
+      );
+    }
+
+    // Check if wallet is connected
+    else if (status === WalletStatus.WALLET_CONNECTED) {
+      return (
+        <button
+          type="button"
+	  onClick={() => disconnect()}
+	  className="cta-button connect-wallet-button"
+        >
+	  Disconnect
+	</button>
+      );
+    }
+  };
 
   return (
     <main className="App">
       <header>
         <div className="header-titles">
-          <h1>⚔ Goblin War ⚔</h1>
-          <p>Only you can save us from Goblin town</p>
+          <h1>🎮 Gamers War 🎮</h1>
+          <p>Only you can save us from the tryhard gamers!</p>
         </div>
-
+        <WalletAddress />
       </header>
+      
+      {status === WalletStatus.WALLET_NOT_CONNECTED && (
+        <div>
+          <img
+            src="https://media.giphy.com/media/l0HlPtbGpcnqa0fja/giphy.gif"
+            alt="SP gif"
+          />
+        </div>
+      )}
+	
+      {status === WalletStatus.WALLET_CONNECTED && (
+	<div className="game-menu-container">
+	  <Menu />
+        </div>
+      )}
 
-      <div>
-        <img
-          src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
-          alt="Goblin gif"
-        />
-      </div>
+      {renderConnectButton()}
     </main>
   );
 }
